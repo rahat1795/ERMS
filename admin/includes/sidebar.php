d64f3a0dc11f998ab44ecd6524162794780d4be9
rahat1795 <1830831@iub.edu.bd>
2021-09-13 04:38:13 +0600

    initial commit

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,40 @@
+  <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+        
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="myprofile.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">ERMS Admin</div>
+      </a>
+
+      <hr class="sidebar-divider my-0">
+
+      <!-- Dashboard -->
+      <li class="nav-item">
+        <a class="nav-link" href="welcome.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li>
+
+      <hr class="sidebar-divider">
+
+      <div class="sidebar-heading">
+        Interface
+      </div>
+     
+
+      <li class="nav-item">
+        <a class="nav-link" href="allemployees.php">
+          <i class="fas fa-fw fa-chart-area"></i>
+          <span>All Employees</span></a>
+      </li>
+      
+      <hr class="sidebar-divider d-none d-md-block">
+
+        
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+
+    </ul>
